70054923e8d4534a4382acbaa79e6f95284a7380
DHARUN <159411407+DHARUN437@users.noreply.github.com>
2025-04-03 11:16:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,27 +11,26 @@ query = st.text_input("Ask a question about sales data:")
 
 if query:
     if "what are the top selling products" in query.lower():
-        st.image("assets\output.png")
+        st.image(r"assets\output.png")
     
     elif "who is the most returned customer" in query.lower():
-       st.image('assets\image.png')
+       st.image(r'assets\image.png')
     
     elif "what is the average order value" in query.lower():
         avg_order_value = df["Total Price"].mean()
         
         st.write("### Average Order Value")
         st.write(avg_order_value)
-       # st.metric(label="Average Order Value (₹)", value=round(avg_order_value, 2))
     
     elif "what are the most profitable products" in query.lower():
         
         st.write("### Top 5 Most Profitable Products are :")
-        st.image("assets\profit.png")
+        st.image(r"assets\profit.png")
     
     elif "who is the most frequent customer" in query.lower():
                 
         st.write("### The most Frequent Customers are :")
-        st.image('assets\customers.png')
+        st.image(r'assets\customers.png')
        
     
     else:
